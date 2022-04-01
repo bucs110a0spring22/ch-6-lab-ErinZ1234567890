030addd21f31681cf038000f2578d98cc10cfe1a
Erin Zhao <erinzhao8100@gmail.com>
2022-04-01 02:13:39 +0000

    did lab

--- README.md
+++ README.md
@@ -8,18 +8,22 @@
 _Replace anything surrounded by the `< >` symbols._
 
 ## SUMMARY:
- < Provide a short description of your program's functionality. No more than a couple sentences. >
+Based on the Collatz Conjecture, counts how many iterations it takes for any given number to 1 following the algorithm: n = n/2 if n is even, n = 3n+1 if n is odd.
+Asks user to input an upperbound for inputs
+Graphs the integer inputted along with the number of iterations needed for that integer to reach 1. Also displays the maximum number of iterations within upperbound.
+Prints the input number and the iterations in the seq3np1 at the end.
 
 ## GRACE DAYS
-Grace days used for this assignment: < # >
+Grace days used for this assignment: 3
 
-Grace days remaining: < # >/5
+Grace days remaining: 2/5
 
 ## KNOWN BUGS AND INCOMPLETE PARTS:
- < What parts of the project you were not able to complete >
+None
 
 ## REFERENCES:
- < List any outside resources used >
+Python Turtle Library to look up write()
+Python Library to look up smart strings in py
 
 ## MISCELLANEOUS COMMENTS:
- < Anything you would like the grader to know >
+None
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import turtle
 
 def seq3np1(n):
     """
@@ -5,14 +6,59 @@ def seq3np1(n):
         args: n (int) starting value for 3n+1 sequence
         return: None
     """
+    count = 0
     while(n != 1):
-        print(n)
+        #print(n)
+        count += 1
         if(n % 2) == 0:        # n is even
             n = n // 2
         else:                 # n is odd
             n = n * 3 + 1
-    print(n)                  # the last print is 1
+    #print(n)                  # the last print is 1
+    return count
+
+def drawGraph(uB):
+  """
+  Draws Graph of seq3np1 values and displays max value so far in top left. window grows along with graph.
+  """
+  
+  maxSoFar = 0 #keeps track of max value of seq3np1 so far
+  graphing = turtle.Turtle() #draws the graph of seq3np1 vals
+  graphing.goto(0, 0)
+  writer = turtle.Turtle() #writes max val of seq3np1 within bounds
+  window = turtle.Screen()
+  window.setworldcoordinates(0,0,10,10)
+  graphing.down()
+  writer.up()
+  
+  for i in range(1, uB+1):
+    result = seq3np1(i)
+    print("result {}".format(result))
+    
+    if (result > maxSoFar):
+      maxSoFar = result
+      writer.clear()
+      writer.goto(0,maxSoFar)
+      writer.write("Maximum so far: {}, {} ".format(i, result))
+      
+    window.setworldcoordinates(0, 0, i+10, maxSoFar+10)
+    graphing.goto(i, result)
+  print("end)")
+  #window.exitonclick()
 
 def main():
-	seq3np1(3)
+  window = turtle.Screen()
+  print("test")
+  upBound = (int)(input("Please input an upper bound: ")) 
+  
+  if(upBound > 0):
+    drawGraph(upBound)
+    for start in range(1, upBound+1):
+      seq3np1(start)
+      print("start: {}".format(start))
+      print("seq3np1: {}".format(seq3np1(start)))
+  else:
+    print("please restart the program and input a positive number for the upper bound.")
+    
+  window.exitonclick()
 main()
